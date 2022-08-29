349048600d3bc6abb07b5127d534a7f284bf07bd
JongeunKeum <jon9eun@gmail.com>
2022-08-29 15:31:32 +0900

    Feat: add comments to login button and main page

--- srcs/frontend/src/components/login/Login.jsx
+++ srcs/frontend/src/components/login/Login.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Button } from '@chakra-ui/react';
 
+/**
+ * 로그인 버튼 만드는 함수
+ * 
+ * @param title : 버튼 텍스트
+ * @param onClick : 버튼 눌렀을 때 발생시킬 이벤트
+ * @returns 로그인 버튼
+ */
+
 function LoginButton(props) {
 	const { title, onClick } = props;
 
