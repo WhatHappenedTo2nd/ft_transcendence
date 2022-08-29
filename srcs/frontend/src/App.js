6cfad4d36f1651981784d115860f7bad3b0c86f6
JongeunKeum <jon9eun@gmail.com>
2022-08-29 16:35:32 +0900

    Feat: add choice page after login - profile and chatting

--- srcs/frontend/src/App.js
+++ srcs/frontend/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import MainPage from './components/pages/MainPage';
+import ChoicePage from './components/pages/ChoicePage';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<MainPage />} />
+          <Route path="choice" element={<ChoicePage />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
