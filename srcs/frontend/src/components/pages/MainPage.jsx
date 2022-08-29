7afdf50f12964936faa4c24956a8087436aa7d5c
JongeunKeum <jon9eun@gmail.com>
2022-08-29 15:31:32 +0900

    Feat: add comments to login button and main page

--- srcs/frontend/src/components/pages/MainPage.jsx
+++ srcs/frontend/src/components/pages/MainPage.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import LoginButton from "../login/Login";
 
+/**
+ * 메인 페이지에서 로고와 로그인 버튼을 함께 가지고 있는 스타일 태그
+ */
 const Wrapper = styled.div`
 	padding: 16px;
 	display: flex;
@@ -11,6 +14,12 @@ const Wrapper = styled.div`
 	justify-content: center;
 `;
 
+/**
+ * 메인 페이지
+ * 
+ * @param {*} props 
+ * @returns 로고와 로그인 버튼을 묶고 있는 Wrapper
+ */
 function MainPage(props) {
 	const {} = props;
 
