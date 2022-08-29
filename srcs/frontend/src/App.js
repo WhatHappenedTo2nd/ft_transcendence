bed8287ff4d02808c2db1a0f07366082c3cfe1e2
JongeunKeum <jon9eun@gmail.com>
2022-08-29 16:08:51 +0900

    Feat: add ChakraProvider to use chakra-ui

--- srcs/frontend/src/App.js
+++ srcs/frontend/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import MainPage from './components/pages/MainPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
