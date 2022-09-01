a0a243edba7f46ed7ae3d9d866711cca81c00f22
JongeunKeum <jon9eun@gmail.com>
2022-09-01 20:06:52 +0900

    Feat: add auth module, controller, service

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
