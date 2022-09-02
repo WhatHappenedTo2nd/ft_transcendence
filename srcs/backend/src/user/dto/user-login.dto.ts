a731b0739b5b7d906345fb7f36bdbb63a20c7f1a
JongeunKeum <jon9eun@gmail.com>
2022-09-02 14:44:15 +0900

    Feat: install class-validator to check data validation and use in dto

--- srcs/backend/src/user/dto/user-login.dto.ts
+++ srcs/backend/src/user/dto/user-login.dto.ts
@@ -1,8 +1,20 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 /**
  * 유저가 로그인할 때 필요한 데이터 객체 dto
  */
 export class UserLoginDto {
+	@IsString()
 	intra_id: string;
+
+	@IsString()
+	@MinLength(4)
+	@MaxLength(20)
+	@Matches(/^[a-zA-Z0-9]*$/, {
+		message: 'password only accepts english and number'
+	})
 	nickname: string;
+
+	@IsString()
 	avatar: string;
 }
