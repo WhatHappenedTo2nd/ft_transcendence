4899c55482ddd1178918de9481fba22e8bf61f73
JongeunKeum <jon9eun@gmail.com>
2022-09-02 11:46:32 +0900

    Feat: add user repository and dto

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
