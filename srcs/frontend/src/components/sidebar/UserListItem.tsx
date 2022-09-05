d8fbf55bbd371989f5fdc8f4d8d6c59db6ac48be
stopminbeom <mseo@student.42seoul.kr>
2022-09-05 22:20:48 +0900

    Fix: UserList 아바타 표시방식 변경

--- srcs/frontend/src/components/sidebar/UserListItem.tsx
+++ srcs/frontend/src/components/sidebar/UserListItem.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-query'
 import { getUserList } from '../../api/api';
+import { Box, Avatar, AvatarBadge, Container } from '@chakra-ui/react'
 
 interface UserListProps {
 	id: number;
@@ -18,14 +19,18 @@ function OnlineUserListItem() {
 	return (
 		<div>
 			{UserList?.map((user) => {
-				if (user.is_online === true) {
+				if (user.is_online) {
 					return (
-						<div key={user?.id}>
-							<img alt="UserAvatar" src={user?.avatar} />
-							<div>
+						<Container key={user?.id} display='flex'>
+							<Box boxSize='10' justifyContent='center'>
+								<Avatar size='sm' src={user?.avatar}>
+									<AvatarBadge boxSize='1em' bg='green.500' />
+								</Avatar>
+							</Box>
+							<Box justifyContent='flex-end'>
 								<span>{user?.nickname}</span>
-							</div>
-						</div>
+							</Box>
+						</Container>
 					);
 				}
 				})}
