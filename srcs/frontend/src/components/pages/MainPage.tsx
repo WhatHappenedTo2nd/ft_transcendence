e8913f13437cfe4de0ae34e43df1da2d00e34aaa
JongeunKeum <jon9eun@gmail.com>
2022-09-07 12:31:36 +0900

    Feat: link to 42 login uri when click the login button

--- srcs/frontend/src/components/pages/MainPage.tsx
+++ srcs/frontend/src/components/pages/MainPage.tsx
@@ -2,14 +2,16 @@ import { useNavigate } from "react-router-dom";
 import LoginButton from "../login/Login";
 import { Container, Spacer } from '@chakra-ui/react';
 
+const login = () => {
+	window.location.href = `${process.env.REACT_APP_FT_LOGIN_URI}`;
+};
+
 /**
  * 메인 페이지
  * 
  * @returns 로고와 로그인 버튼을 묶고 있는 Wrapper
  */
 function MainPage() {
-	const navigate = useNavigate();
-
 	const fontStyle = {
 		fontFamily : "Establish",
 		fontSize : "72px",
@@ -22,10 +24,7 @@ function MainPage() {
 			<Spacer margin="48px" />
 			<LoginButton
 				title="Login"
-				onClick={() => {
-					navigate("/choice");
-					console.log("login success");
-				}}
+				onClick={login}
 				/>
 			<Container h="3xs" />
 		</Container>
