67447bf285018ebedf06e66afcfd1342538a13af
inyang <inyang@student.42seoul.kr>
2022-09-07 17:34:20 +0900

    mypage init && get loginuserdata by query

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider} from '@chakra-ui/react';
 import MainPage from './components/pages/MainPage';
 import ChoicePage from './components/pages/ChoicePage';
+import MyPage from './components/pages/MyPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route index element={<MainPage />} />
             <Route path="choice" element={<ChoicePage />} />
+            <Route path="mypage" element={<MyPage />} />
           </Routes>
       </BrowserRouter>
     </ChakraProvider>
