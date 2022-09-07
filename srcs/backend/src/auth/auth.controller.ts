ce165d5f37f6443e693305462b4cc0081cf8c9d5
JongeunKeum <jon9eun@gmail.com>
2022-09-07 12:07:55 +0900

    Feat: auth controller... add GET method to 42api callback

--- srcs/backend/src/auth/auth.controller.ts
+++ srcs/backend/src/auth/auth.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Redirect, Req, Res } from '@nestjs/common';
+import { UserLoginDto } from 'src/user/dto/user-login.dto';
+import { UserRepository } from 'src/user/user.repository';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+import * as config from 'config';
 
+const authConfig = config.get('auth');
+
+/**
+ * auth 경로 그룹
+ * 42API로 유저 인증 혹은 2단계 인증 관련한 작업 처리
+ */
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+	constructor(
+		private userRepository: UserRepository,
+		private jwtService: JwtService
+	) {}
+
+	@Get('42')
+	async ftCallback(@Req() req, @Res() res: Response): Promise<void> {
+		const { intra_id, nickname, avatar } = req.user;
+		const user = await this.userRepository.findOne({ where: { intra_id } });
+
+		if (!user) {
+			const userLoginDto: UserLoginDto = { intra_id, nickname, avatar };
+			await this.userRepository.createUser(userLoginDto);
+		}
+		const payload = { intra_id }
+		const accessToken = await this.jwtService.sign(payload);
+		// accessToken을 프론트로 넘겨줘야 함(유저가 로컬 스토리지에 가지고 있게)
+	}
+}
--- srcs/backend/src/auth/auth.module.ts
+++ srcs/backend/src/auth/auth.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/user/user.repository';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import * as config from 'config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConfig.secret,
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [JwtStrategy]
 })
 export class AuthModule {}
