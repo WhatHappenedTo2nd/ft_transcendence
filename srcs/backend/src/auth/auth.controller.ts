48fc228a4025e610a4c3eb85f0705cccdca8baa4
JongeunKeum <jon9eun@gmail.com>
2022-09-08 19:28:47 +0900

    Feat: add cookie to response, add comments

--- srcs/backend/src/auth/auth.controller.ts
+++ srcs/backend/src/auth/auth.controller.ts
@@ -1,11 +1,9 @@
-import { Controller, Get, Redirect, Req, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { UserLoginDto } from 'src/user/dto/user-login.dto';
 import { UserRepository } from 'src/user/user.repository';
 import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
-import * as config from 'config';
-
-const authConfig = config.get('auth');
+import { AuthGuard } from '@nestjs/passport';
 
 /**
  * auth 경로 그룹
@@ -19,16 +17,24 @@ export class AuthController {
 	) {}
 
 	@Get('42')
+	@UseGuards(AuthGuard('42'))
 	async ftCallback(@Req() req, @Res() res: Response): Promise<void> {
-		const { intra_id, nickname, avatar } = req.user;
+		// request로 user의 intra_id와 avatar 받아옴
+		const { intra_id, avatar } = req.user;
+		// 디비에서 intra_id로 해당 유저가 있는지 찾음
 		const user = await this.userRepository.findOne({ where: { intra_id } });
-
+		// 유저가 디비에 없으면 생성함
 		if (!user) {
-			const userLoginDto: UserLoginDto = { intra_id, nickname, avatar };
+			// dto로 데이터 체크
+			const userLoginDto: UserLoginDto = { intra_id, avatar };
+			// 데이터를 createUser 함수로 보내서 디비에 저장하게 함
 			await this.userRepository.createUser(userLoginDto);
 		}
+		// 토큰에 넣어줄 정보. intra_id만 넣어줌
 		const payload = { intra_id }
+		// 토큰 생성
 		const accessToken = await this.jwtService.sign(payload);
 		// accessToken을 프론트로 넘겨줘야 함(유저가 로컬 스토리지에 가지고 있게)
+		res.cookie('token', accessToken);
 	}
 }
