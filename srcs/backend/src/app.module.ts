55eb16eae959056bd4e9068dcd01ca250c310507
JongeunKeum <jon9eun@gmail.com>
2022-09-09 17:16:47 +0900

    Test: call 42api, get user info and token... using ejs

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -5,10 +5,23 @@ import { AppService } from './app.service';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
+    // TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRoot({
+      type: dbConfig.type,
+      host: dbConfig.host,
+      port: dbConfig.port,
+      username: dbConfig.username,
+      password: dbConfig.password,
+      database: dbConfig.database,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true,
+    }),
     AuthModule,
     UserModule,
   ],
