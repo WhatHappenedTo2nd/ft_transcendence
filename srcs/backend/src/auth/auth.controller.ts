0d6b0e3e9117b6bf89e44a57b10a88515496e444
JongeunKeum <jon9eun@gmail.com>
2022-09-09 18:02:59 +0900

    Done: login with 42api and save token using cookie... still have view ejs to test... it will be changed later

--- srcs/backend/src/auth/auth.controller.ts
+++ srcs/backend/src/auth/auth.controller.ts
@@ -21,25 +21,26 @@ export class AuthController {
 	@Get('42')
 	@UseGuards(AuthGuard('42'))
 	async ftCallback(@Req() req, @Res() res: Response): Promise<void> {
-		console.log(req.user);
-		// // request로 user의 intra_id와 avatar 받아옴
-		// const { intra_id, avatar } = req.user;
-		// // 디비에서 intra_id로 해당 유저가 있는지 찾음
-		// const user = await this.userRepository.findOne({ where: { intra_id } });
-		// // 유저가 디비에 없으면 생성함
-		// if (!user) {
-		// 	// dto로 데이터 체크
-		// 	const userLoginDto: UserLoginDto = { intra_id, avatar };
-		// 	// 데이터를 createUser 함수로 보내서 디비에 저장하게 함
-		// 	await this.userRepository.createUser(userLoginDto);
-		// }
-		// // 토큰에 넣어줄 정보. intra_id만 넣어줌
-		// const payload = { intra_id };
-		// // 토큰 생성
-		// const accessToken = await this.jwtService.sign(payload);
-		// // accessToken을 프론트로 넘겨줘야 함(유저가 로컬 스토리지에 가지고 있게)
-		// res.cookie('token', accessToken);
-		// res.status(302).redirect('http://localhost:3000');
+		// console.log(req.user);
+		// request로 user의 intra_id와 avatar 받아옴
+		const { intra_id, avatar } = req.user;
+		// 디비에서 intra_id로 해당 유저가 있는지 찾음
+		const user = await this.userRepository.findOne({ where: { intra_id } });
+		// 유저가 디비에 없으면 생성함
+		if (!user) {
+			// dto로 데이터 체크
+			const userLoginDto: UserLoginDto = { intra_id, avatar };
+			// 데이터를 createUser 함수로 보내서 디비에 저장하게 함
+			await this.userRepository.createUser(userLoginDto);
+		}
+		// 토큰에 넣어줄 정보. intra_id만 넣어줌
+		const payload = { intra_id };
+		// 토큰 생성
+		const accessToken = await this.jwtService.sign(payload);
+		// accessToken을 cookie에 저장함
+		res.cookie('token', accessToken);
+		// 후에 프론트 choice 페이지로 redirect
+		res.status(302).redirect('http://localhost:3000');
 	}
 
 }
