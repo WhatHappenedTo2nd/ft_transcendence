7fbb188415ecc88af2f3b167a58329abc4370596
inyang <inyang@student.42seoul.kr>
2022-09-13 21:35:32 +0900

    FEAT: GetUserById api make-ing

--- srcs/backend/src/user/user.controller.ts
+++ srcs/backend/src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+	constructor(private userService: UserService){}
+
+	@Get('')
+	getUserList(): Promise<User[]> {
+		return this.userService.getUserList();
+	}
+
+	@Get('/:id')
+	getUserById(@Param('id') id:number) {
+		return this.userService.getUserById(id);
+	}
+
+}
--- srcs/backend/src/user/user.service.ts
+++ srcs/backend/src/user/user.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/user.entity';
+import { UserRepository } from 'src/user/user.repository';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(
+		@InjectRepository(UserRepository)
+		private userRepository: UserRepository,
+	){}
+
+	//유저 전체 리스트
+	async getUserList(): Promise<User[]> {
+		const users = await this.userRepository.find({});
+		return users;
+	}
+
+	async getUserById(id: number): Promise<User> {
+		const user = await this.userRepository.findOne(id);
+		if (!user) {
+			throw new NotFoundException(`Can't find User id ${id}`)
+		}
+		return user;
+	}
+}
