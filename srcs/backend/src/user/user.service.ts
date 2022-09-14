92b190f9d7175e680b08822833a8854f1dec480b
inyang <inyang@student.42seoul.kr>
2022-09-14 21:20:14 +0900

    FIX: add docker npm i socket.io-client

--- srcs/backend/src/user/user.service.ts
+++ srcs/backend/src/user/user.service.ts
@@ -1,5 +1,5 @@
 //유저 전체 리스트
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User} from './user.entity'
@@ -21,8 +21,21 @@ export class UserService {
 	async getUserById(id: number): Promise<User> {
 		const user = await this.userRepository.findOne({where: {id}});
 		if (!user) {
-			throw new NotFoundException(`Can't find User id ${id}`)
+			throw new NotFoundException(`해당 유저를 찾을 수 없습니다`)
 		}
 		return user;
 	}
+
+	// async updateUserNickname(id: number, nickname: string): Promise<User> {
+	// 	const user = await this.getUserById(id);
+	// 	if (!user) {
+	// 		throw new BadRequestException(`해당 유저를 찾을 수 없습니다.`)
+	// 	}
+	// 	if (!nickname) {
+	// 		throw new BadRequestException(`변경된 내용이 없습니다.`)
+	// 	}
+	// 	// if (nickname)
+
+	// 	// user.nickname = 
+	// }
 }
