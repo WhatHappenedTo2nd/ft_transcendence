08badca5ef9ed69703376da5b32e68cf47ea1178
JongeunKeum <jon9eun@gmail.com>
2022-09-17 15:34:37 +0900

    Fix: modify accessToken expire time to 3 hours

--- srcs/backend/src/auth/auth.controller.ts
+++ srcs/backend/src/auth/auth.controller.ts
@@ -47,7 +47,7 @@ export class AuthController {
 		// 토큰 생성
 		const accessToken = await this.jwtService.sign(payload);
 		// accessToken을 cookie에 저장함
-		res.cookie('token', accessToken);
+		res.cookie('accessToken', accessToken);
 		res.cookie('intra_id', intra_id);
 		// 후에 프론트 choice 페이지로 redirect
 		res.status(302).redirect('http://localhost:3000/choice');
