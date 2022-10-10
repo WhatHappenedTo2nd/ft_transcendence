c3c0123937cefbd89d580d7f88f13100803e9708
stopminbeom <mseo@student.42seoul.kr>
2022-10-10 17:14:28 +0900

    Fix: 방 생성 시 방 이름 표시 안 되던 문제 수정 완료

--- srcs/backend/src/chat/chat.service.ts
+++ srcs/backend/src/chat/chat.service.ts
@@ -41,7 +41,6 @@ export class ChatService {
 	async getRoomUserList(path: string): Promise<ChatUserDefaultDto[]> {
 		const id: number = Number(path);
 		const room = await this.chatRepository.findOneById(id);
-		console.log(room);
 		const roomuser = await this.chatUserRepository.find({
 			relations: {
 				user_id: true,
