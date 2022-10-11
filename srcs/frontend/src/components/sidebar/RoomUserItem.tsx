e21827cc4e9b980f4742f8ab84fbac4520c4768d
stopminbeom <mseo@student.42seoul.kr>
2022-10-11 21:00:13 +0900

    Fix: admin 상태일 때 host한테 나쁜짓 못하게...

--- srcs/frontend/src/components/sidebar/RoomUserItem.tsx
+++ srcs/frontend/src/components/sidebar/RoomUserItem.tsx
@@ -1,14 +1,16 @@
 import { Box, Avatar, AvatarBadge, HStack } from '@chakra-ui/react'
 import UserContextMenu from './contextmenu/UserContextmenu';
 
-function UserItem(props: any) {
-	const { user, mode } = props;
+function RoomUserItem(props: any) {
+	const { user, mode, myrole, muted, targetrole } = props;
 	return (
 		<UserContextMenu
 		userId={user.id}
 		name={user.nickname}
 		mode={mode}
-		>
+		myrole={myrole}
+		muted={muted}
+		targetrole={targetrole}>
 			<HStack>
 			<Box boxSize='10' justifyContent='center' marginStart="2" marginTop="0.5" padding="1" >
 				<Avatar size='sm' src={user?.avatar}>
@@ -25,4 +27,4 @@ function UserItem(props: any) {
 	);
 }
 
-export default UserItem;
+export default RoomUserItem;
