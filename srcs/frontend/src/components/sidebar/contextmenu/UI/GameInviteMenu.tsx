40711d4358f1d6e29f14073679f8d5f34a319ad6
stopminbeom <mseo@student.42seoul.kr>
2022-10-12 01:15:58 +0900

    Feat: 게임 초대 추가

--- srcs/frontend/src/components/sidebar/contextmenu/UI/GameInviteMenu.tsx
+++ srcs/frontend/src/components/sidebar/contextmenu/UI/GameInviteMenu.tsx
@@ -1,5 +1,5 @@
 import { MenuItem, Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useQuery } from "react-query";
 import { useNavigate, useParams } from 'react-router-dom';
 import { getBlockList, getWhereAreYou } from '../../../../api/api';
@@ -7,17 +7,12 @@ import { getCookie } from '../../../../api/cookieFunc';
 import { socket } from '../../../../App';
 import ICreateRoomResponse from '../../../interface/IChatProps';
 import useWarningAlert from '../../../../hooks/useWarnigAlert';
-import IChatListProps from '../../../interface/IChatListProps';
 import IFriendProps from '../../../interface/IFriendProps';
 
 export default function GameInviteMenu({label, target}: {label: string; target: string;}) {
 	const navigate = useNavigate();
 	const { setError, WarningDialogComponent } = useWarningAlert();
-	const [password, setPassword] = useState('');
-	const { data: chat } = useQuery<IChatListProps>(['findroom', target], () => getWhereAreYou(target));
 	const { data: block } = useQuery<IFriendProps[]>('block', getBlockList);
-	const roomId = Number(useParams<'roomName'>().roomName);
-	const [userRes, setUserRes] = useState('');
 
 	let blockCheck = false;
 	for (let i = 0; i < block!.length; i++)
@@ -27,7 +22,7 @@ export default function GameInviteMenu({label, target}: {label: string; target: 
 	}
 
 	// console.log("label: ", label, "target: ", target);
-	const onJoinRoom = (roomName?: string) => () => {
+	const onJoinRoom = () => () => {
 		if (blockCheck){
 			setError({
 				headerMessage: '입장 실패',
@@ -35,55 +30,13 @@ export default function GameInviteMenu({label, target}: {label: string; target: 
 			})
 		}
 		else {
-			console.log("게임 초대 버튼으로 게임 방을 만듭니다.");
-			socket.emit('leave-room', { roomId, roomName, userIntraId: getCookie("intra_id") }, () => {
-				console.log("기존에 있던 방을 나왔습니다.");
-			});
-			setPassword(target);
-			console.log("password를 확인합니다.", password);
-			socket.emit('create-room', { roomName: target, password, userIntraId: getCookie("intra_id") }, (response: ICreateRoomResponse) => {
-				console.log("새로운 방을 만듭니다.");
-				if (!response.success)
-				return alert(response.payload);
-				navigate(`/room/${response.payload}`);
-			});
-			console.log("새로 만든 방에 들어왔습니다.");
-			console.log("==========================");
-			socket.emit('invite-room', { roomId, roomName, name: target }, (response: ICreateRoomResponse) => {
-				console.log("타겟이 기존에 있던 방을 나왔습니다.");
-				if (!response.success)
-				return alert(response.payload);
-				setUserRes(response.payload);
-				console.log("invite room 확인", response.payload);
-			});
-
-			console.log("userRes를 확인합니다.", userRes);
-			socket.emit('join-room', {roomName, userIntraId: userRes}, (response: ICreateRoomResponse) => {
-				console.log('join-room를 실행합니다.');
-				if (!response.success)
-				return alert(response.payload);
-				console.log("joinRoom: payload", response.payload);
-				navigate(`/room/${response.payload}`);
-			});
+			socket.emit('invite-room', { name: target, userIntraId: getCookie("intra_id")}, () => {});
 		}
 	};
 
-	// useEffect(() => {
-	// 	socket.on('invite-room', (res: ICreateRoomResponse) => {
-	// 		console.log("payload: ", res.payload, "roomName: ", res.roomName);
-	// 		socket.emit('join-room', {roomName: res.roomName, userIntraId: res.payload}, (response: ICreateRoomResponse) => {
-	// 			console.log('join-room를 실행합니다.');
-	// 			if (!response.success)
-	// 			return alert(response.payload);
-	// 			console.log("joinRoom: payload", response.payload);
-	// 			navigate(`/room/${response.payload}`);
-	// 		});
-	// 	});
-	// })
-
 	return (
 		<>
-		<MenuItem onClick={onJoinRoom(chat?.title)}>
+		<MenuItem onClick={onJoinRoom()}>
 			<Text>{label}</Text>
 		</MenuItem>
 		{WarningDialogComponent}
